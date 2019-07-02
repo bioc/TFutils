2ca1c650d9b75ef0e012c776c61929256c1b7853
Vince Carey <stvjc@channing.harvard.edu>
2019-07-02 10:56:02 -0400

    revising fimo_granges to work with various ncol values for GenomicFiles

--- R/fimo_granges.R
+++ R/fimo_granges.R
@@ -19,6 +19,41 @@
 fimo_granges = function (gf, query) 
 {
     rowRanges(gf) = query
+    proctext = function(x) {
+        con = textConnection(x)
+        on.exit(close(con))
+        dtf = read.delim(con, h = FALSE, stringsAsFactors = FALSE, 
+            sep = "\t")
+        colnames(dtf) = c("chr", "start", "end", "rname", "score", 
+            "dir", "pval")
+        ans = with(dtf, GRanges(seqnames = chr, IRanges(start, 
+            end), rname = rname, score = score, dir = dir, pval = pval))
+        ans
+    }
+    nfun = function(ans) lapply(ans, lapply, proctext)
+    ans = reduceByRange(gf, MAP = function(r, f) scanTabix(f, 
+        param = r), REDUCE = nfun)
+    if (ncol(gf) == 1) {  # REDUCE IS NOT INVOKED FOR ncol(fg) == 1
+        ans = lapply(ans, function(z) proctext(z[[1]]))
+        ans = lapply(ans, function(z) {
+          seqinfo(z) = seqinfo(query)
+          z
+        })
+       }
+    else {
+        ans = unlist(ans, recursive = FALSE)
+        ans = lapply(ans, lapply, function(x) {
+            seqinfo(x) = seqinfo(query)
+            x
+        })
+    }
+    names(ans) = rep(colnames(gf), length(query))
+    ans
+}
+
+fimo_granges_OLD = function (gf, query) 
+{
+    rowRanges(gf) = query
     nfun = function(ans) lapply(ans, lapply, function(x) {
         con = textConnection(x)
         on.exit(close(con))
