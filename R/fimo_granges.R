79029b950670610887a88af8e6db59344be38262
Vince Carey <stvjc@channing.harvard.edu>
2019-07-02 08:14:55 -0400

    new fimo_granges method

---
+++ R/fimo_granges.R
@@ -0,0 +1,38 @@
+#' create a list of GRanges for FIMO hits in a GenomicFiles instance, corresponding to a GRanges-based query
+#' @param gf GenomicFiles instance, like fimo16 in TFutils
+#' @param query a GRanges specifying ranges to check for TF binding scores
+#' @return a list of GRanges, produced by GenomicFiles::reduceByRange 
+#' @note Be sure to use `register([BPPARAM])` appropriately.
+#' @examples
+#' if (interactive()) {   # need internet
+#'  # setup -- annotate fimo16 object and create an informative
+#'  # query
+#'  colnames(fimo16) = fimo16$HGNC
+#'  si = GenomeInfoDb::Seqinfo(genome="hg19")["chr17"] # to fix query genome
+#'  myg = GRanges("chr17", IRanges(38.07e6,38.09e6), seqinfo=si)
+#'  requireNamespace("BiocParallel")
+#'  BiocParallel::register(BiocParallel::SerialParam())
+#'  f1 = fimo_granges(fimo16[, c("VDR", "POU2F1")], myg)
+#'  f1
+#' }
+#' @export
+fimo_granges = function (gf, query) 
+{
+    rowRanges(gf) = query
+    nfun = function(ans) lapply(ans, lapply, function(x) {
+        con = textConnection(x)
+        on.exit(close(con))
+        dtf = read.delim(con, h = FALSE, stringsAsFactors = FALSE, 
+            sep = "\t")
+        colnames(dtf) = c("chr", "start", "end", "rname", "score", 
+            "dir", "pval")
+        ans = with(dtf, GRanges(seqnames = chr, IRanges(start, 
+            end), rname = rname, score = score, dir = dir, pval = pval))
+        ans
+    })
+    ans = reduceByRange(gf, MAP = function(r, f) scanTabix(f, 
+        param = r), REDUCE = nfun)
+    ans = unlist(ans, recursive=FALSE)
+    names(ans) = rep(colnames(gf), length(query))
+    lapply(ans, lapply, function(x) {seqinfo(x) = seqinfo(query); x})
+}
