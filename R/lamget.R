268035b191ee12c621cd5c75f9334a369f51dadd
Vince Carey <stvjc@channing.harvard.edu>
2020-11-04 10:41:48 -0500

    dealing with change to the URL for lambert file at CELL

--- R/lamget.R
+++ R/lamget.R
@@ -1,4 +1,9 @@
+# Update Oct 31 2020?
+cell_url = function() "https://www.cell.com/cms/10.1016/j.cell.2018.01.029/attachment/ede37821-fd6f-41b7-9a0e-9d5410855ae6/mmc2.xlsx"
+
 #' acquire the Excel spreadsheet content for table S1 of Lambert et al. Cell 2018, "The Human Transcription Factors"
+#' @import BiocFileCache
+#' @import readxl
 #' @param cache a BiocFileCache instance
 #' @return a tbl_df
 #' @note This will download the spreadsheet if not found in `cache`.
@@ -6,11 +11,11 @@
 #' if (interactive()) retrieve_lambert_main()
 #' @export
 retrieve_lambert_main = function(cache=BiocFileCache::BiocFileCache(ask=FALSE)) {
- uuid_element = "88c0eca1-66f9-4068-b02e-bd3d55144f79" # from Cell supplement
+ uuid_element = "ede37821-fd6f"
  peek = BiocFileCache::bfcquery(cache, uuid_element)
  if(length(peek$rpath)==0) 
-   bfcadd(cache, 
-    "https://www.cell.com/cms/10.1016/j.cell.2018.01.029/attachment/88c0eca1-66f9-4068-b02e-bd3d55144f79/mmc2.xlsx") 
+   BiocFileCache::bfcadd(cache, 
+    cell_url())
  peek = try(BiocFileCache::bfcquery(cache, uuid_element))
  if (inherits(peek, "try-error") || length(peek$rpath)==0) stop('could not retrieve xlsx')
  ans = suppressMessages(suppressWarnings(readxl::read_xlsx(peek$rpath, skip=1, sheet=2)))
